379c4f6cf47efbd2563b6ca8e9cbc1200d5b3777
Rafael Josh Sanchez <rafaeljoshsanchez11@gmail.com>
2024-08-22 11:49:15 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+Fnumber = input ("Enter First Number: ")
+print("First Number: " + Fnumber)
+
+Snumber = input ("Enter Second Number: ")
+print("Second Number: " + Snumber)
+
+if Fnumber < Snumber:
+    print(Fnumber + " is greater than " + Snumber)
+elif Fnumber > Snumber:
+    print(Fnumber + " is lower  than " + Snumber)
+else:
+    print(Fnumber + " is equal to " + Snumber)
+
+# name1 = input("Enter classmate name 1: ")
+# name2 = input("Enter classmate name 2: ")
+# name3 = input("Enter classmate name 3: ")
+#
+# print("Your classmates are: " + name1 + ", " + name2 + "," + " & " + "" + name3)
